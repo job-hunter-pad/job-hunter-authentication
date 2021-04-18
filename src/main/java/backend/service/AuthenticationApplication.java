fd27fea0ff63c621794d5e4aa79f9f9ddf74a679
Andrei Timar <atimar999@gmail.com>
2021-04-18 22:02:02 +0300

    Added Profile Page Photo

--- src/main/java/backend/service/AuthenticationApplication.java
+++ src/main/java/backend/service/AuthenticationApplication.java
@@ -1,6 +1,7 @@
 package backend.service;
 
 import backend.service.authentication.repository.UserRepository;
+import backend.service.profile.repository.ProfilePhotoRepository;
 import backend.service.profile.repository.ProfileRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +9,10 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}, scanBasePackages = {"backend.service.authentication", "backend.service.profile"})
-@EnableMongoRepositories(basePackageClasses = {UserRepository.class, ProfileRepository.class})
+@EnableMongoRepositories(basePackageClasses = {UserRepository.class, ProfileRepository.class, ProfilePhotoRepository.class})
 public class AuthenticationApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(AuthenticationApplication.class, args);
     }
-
 }
--- src/main/java/backend/service/profile/service/ProfileService.java
+++ src/main/java/backend/service/profile/service/ProfileService.java
@@ -3,7 +3,9 @@ package backend.service.profile.service;
 import backend.service.authentication.model.User;
 import backend.service.profile.model.Review;
 import backend.service.profile.model.UserProfile;
+import backend.service.profile.model.UserProfilePhoto;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,4 +19,8 @@ public interface ProfileService {
     Optional<UserProfile> updateProfile(UserProfile userProfile);
 
     UserProfile addReview(String userId, Review review);
+
+    Optional<UserProfilePhoto> getUserProfilePhoto(String userId);
+
+    UserProfilePhoto updateProfilePhoto(UserProfilePhoto userProfilePhoto);
 }
